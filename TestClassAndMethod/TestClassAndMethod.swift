f87c005a70c3a4e5cbdb735a1fa070a87c0b71da
Ravi Parmar <ravi.parmar@zebpay.com>
2018-09-11 16:36:55 +0530

    commit

---
+++ TestClassAndMethod/TestClassAndMethod.swift
@@ -0,0 +1,14 @@
+//
+//  TestClassAndMethod.swift
+//  Alamofire
+//
+//  Created by Ravi Parmar on 11/09/18.
+//
+
+import Foundation
+
+public class DoSomethingClass{
+    public func doSomethingMethod(){
+        print("Print this in console")
+    }
+}
