4e95af9bb431643951bd1795f5c4cfd4de1b432e
Ravi Parmar <ravi.parmar@zebpay.com>
2018-09-12 16:52:49 +0530

    commit

--- iOSTest1509/Classes/TestClassAndMethod/TestClassAndMethod.swift
+++ iOSTest1509/Classes/TestClassAndMethod/TestClassAndMethod.swift
@@ -12,3 +12,16 @@ public class DoSomethingClass{
         print("Print this in console")
     }
 }
+
+
+public class Foo {
+  public  var name: String?           // instance property
+    static var all = [Foo]()    // static type property
+  public  class var comp: Int {       // computed type property
+        return 42
+    }
+    
+  public  class func alert() {        // type method
+        print("There are \(all.count) foos")
+    }
+}
